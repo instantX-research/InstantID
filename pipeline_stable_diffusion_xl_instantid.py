f3e9588f66db26b967398dd4a6d8729768bec471
InstantX <153269709+ResearcherXman@users.noreply.github.com>
2024-01-22 19:30:51 +0800

    Update pipeline_stable_diffusion_xl_instantid.py

--- pipeline_stable_diffusion_xl_instantid.py
+++ pipeline_stable_diffusion_xl_instantid.py
@@ -42,15 +42,7 @@ from diffusers.utils.import_utils import is_xformers_available
 from ip_adapter.resampler import Resampler
 from ip_adapter.utils import is_torch2_available
 
-if is_torch2_available():
-    from ip_adapter.attention_processor import (
-        AttnProcessor2_0 as AttnProcessor,
-    )
-    from ip_adapter.attention_processor import (
-        IPAttnProcessor2_0 as IPAttnProcessor,
-    )
-else:
-    from ip_adapter.attention_processor import AttnProcessor, IPAttnProcessor
+from ip_adapter.attention_processor import AttnProcessor, IPAttnProcessor
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
