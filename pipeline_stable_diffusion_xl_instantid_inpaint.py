b8002bbac67b735b242dbcb619e5c598f8cfa15d
Eun Yeong Jeon <33515697+Eun0@users.noreply.github.com>
2024-05-12 03:23:02 +0900

    Fix encoder_hidden_states (#6)

--- pipeline_stable_diffusion_xl_instantid_inpaint.py
+++ pipeline_stable_diffusion_xl_instantid_inpaint.py
@@ -578,7 +578,7 @@ class StableDiffusionXLInstantIDInpaintPipeline(StableDiffusionXLControlNetInpai
                 noise_pred = self.unet(
                     latent_model_input,
                     t,
-                    encoder_hidden_states=prompt_embeds,
+                    encoder_hidden_states=encoder_hidden_states,
                     cross_attention_kwargs=self.cross_attention_kwargs,
                     down_block_additional_residuals=down_block_res_samples,
                     mid_block_additional_residual=mid_block_res_sample,
